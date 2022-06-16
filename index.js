45a1bfe4d989ee4e18653b20daf551928eec8809
Nyanthie Mayang Gatluak <nyanthie.mayang@student.moringaschool.com>
2022-06-17 00:39:20 +0300

    commit tree 38a720aec1de48d7a41e14ade733178430b47303

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your code here
+function submitData() {
+    const formData = {
+        "name":"Steve",
+        "email":"steve@steve.com",
+    };
+
+    const fetchOptions = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify(formData),
+    };
+
+    let url = "http://localhost:3000/users";
+
+    return fetch(url, fetchOptions)
+    .then(response => response.json())
+    .then(result => document.body.append(result.id))
+    .catch((e) => document.body.append(e.message));
+}
